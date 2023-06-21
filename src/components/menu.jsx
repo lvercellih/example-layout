2f20046f4c28f66170699e73ca8fa4daa03af402
Luis <lvercellih@gmail.com>
2023-06-21 12:30:26 -0500

    Refactorizar componentes

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,25 +3,31 @@ import { FeedbackIcon, FolderIcon, HomeIcon, ReferedProjectsIcon } from './icons
 
 export function Menu() {
   return (
+    <MenuLayout title='Plannifer'>
+      <MenuItem to='/home' icon={<HomeIcon />} label='Home' />
+      <MenuItem to='/refprojects' icon={<ReferedProjectsIcon />} label={<>
+        Refered Projects
+        <MenuBadge content='0' />
+        </>} />
+      <MenuItem to='/resources' icon={<FolderIcon />} label='Resources' />
+      <MenuItem to='/feedback' icon={<FeedbackIcon />} label='Store feedback' />
+      <MenuItem to='/test' icon={<HomeIcon />} label='Test' />
+    </MenuLayout>
+  )
+}
+
+function MenuLayout({ title, children }) {
+  return (
     <div className="relative hidden h-screen shadow-lg lg:block w-80">
       <div className="h-full bg-white dark:bg-gray-700">
         <div className="flex items-center justify-start pt-6 ml-8">
           <p className="text-xl font-bold dark:text-white">
-            Plannifer
+            {title}
           </p>
         </div>
         <nav className="mt-6">
           <div>
-            <MenuItem to='/home' icon={<HomeIcon />} label='Home' />
-            <MenuItem to='/refprojects' icon={<ReferedProjectsIcon />} label={<>
-              Refered Projects
-              <span className="w-4 h-2 p-1 ml-4 text-xs text-gray-400 bg-gray-200 rounded-lg">
-                0
-              </span>
-              </>} />
-            <MenuItem to='/resources' icon={<FolderIcon />} label='Resources' />
-            <MenuItem to='/feedback' icon={<FeedbackIcon />} label='Store feedback' />
-            <MenuItem to='/test' icon={<HomeIcon />} label='Test' />
+            {children}
           </div>
         </nav>
       </div>
@@ -38,5 +44,13 @@ function MenuItem({ label, icon, to }) {
       <span className="text-left">{icon}</span>
       <span className="mx-2 text-sm font-normal">{label}</span>
     </NavLink>
+  )
+}
+
+function MenuBadge({ content }) {
+  return (
+    <span className="w-4 h-2 p-1 ml-4 text-xs text-gray-400 bg-gray-200 rounded-lg">
+      {content}
+    </span>
   )
 }
